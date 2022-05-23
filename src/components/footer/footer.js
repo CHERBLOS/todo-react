04b89612c73941a9137ebeab064c5c90c8ddc024
CHERBLOS <92993083+CHERBLOS@users.noreply.github.com>
2022-05-23 18:08:32 +0300

    Update footer.js

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,56 +1,44 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import STATUS from '../../constants'
+
 import './footer.css'
 
-class Footer extends React.Component {
-  static propTypes = {
-    tasks: PropTypes.arrayOf(PropTypes.shape),
-    deleteTask: PropTypes.func,
-    toggleFilterState: PropTypes.func,
-    filter: PropTypes.string,
-  }
-
-  static defaultProps = {
-    tasks: {},
-    deleteTask: () => {},
-    toggleFilterState: () => {},
-    filter: 'all',
-  }
-
-  buttons = [
-    { name: 'all', label: 'All' },
-    { name: 'active', label: 'Active' },
-    { name: 'completed', label: 'Completed' },
-  ]
-
-  render() {
-    const { tasks, deleteTask, toggleFilterState, filter } = this.props
-    const buttons = this.buttons.map((button) => {
-      const isActive = filter === button.name ? 'selected' : ''
-      return (
-        <li key={button.name}>
-          <button
-            type="button"
-            className={isActive}
-            name={button.name}
-            onClick={(e) => toggleFilterState(e.target.innerText.toLowerCase())}
-          >
-            {button.label}
-          </button>
-        </li>
-      )
-    })
+function Footer({ tasks, deleteTask, toggleFilterState, filter }) {
+  const buttons = Object.keys(STATUS).map((key) => {
+    const isActive = filter === STATUS[key] ? 'selected' : ''
     return (
-      <footer className="footer">
-        <span className="todo-count">{tasks.filter((task) => !task.isCompleted).length} items left</span>
-        <ul className="filters">{buttons}</ul>
-        <button type="button" className="clear-completed" onClick={deleteTask}>
-          Clear completed
+      <li key={key}>
+        <button type="button" className={isActive} name={key} onClick={() => toggleFilterState(STATUS[key])}>
+          {STATUS[key]}
         </button>
-      </footer>
+      </li>
     )
-  }
+  })
+  return (
+    <footer className="footer">
+      <span className="todo-count">{tasks.filter((task) => !task.isCompleted).length} items left</span>
+      <ul className="filters">{buttons}</ul>
+      <button type="button" className="clear-completed" onClick={deleteTask}>
+        Clear completed
+      </button>
+    </footer>
+  )
+}
+
+Footer.propTypes = {
+  tasks: PropTypes.arrayOf(PropTypes.shape),
+  deleteTask: PropTypes.func,
+  toggleFilterState: PropTypes.func,
+  filter: PropTypes.string,
+}
+
+Footer.defaultProps = {
+  tasks: {},
+  deleteTask: () => {},
+  toggleFilterState: () => {},
+  filter: 'All',
 }
 
 export default Footer
